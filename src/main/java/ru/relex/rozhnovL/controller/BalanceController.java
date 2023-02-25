eb17f1cd3706b86a0a16077bcbd8619361159e02
Leo_Rozhnov <levlipetsk375@gmail.com>
2023-02-25 18:54:33 +0300

    regulate Controllers

---
+++ src/main/java/ru/relex/rozhnovL/controller/BalanceController.java
@@ -0,0 +1,159 @@
+package ru.relex.rozhnovL.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import ru.relex.rozhnovL.Services;
+import ru.relex.rozhnovL.entity.Currency;
+import ru.relex.rozhnovL.entity.Curse;
+import ru.relex.rozhnovL.entity.Wallet;
+import ru.relex.rozhnovL.requests.ExchangeCurrencyRequest;
+import ru.relex.rozhnovL.requests.TopUpRequest;
+import ru.relex.rozhnovL.requests.WithdrawCardRequest;
+import ru.relex.rozhnovL.requests.WithdrawWalletRequest;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/balance")
+public class BalanceController {
+
+    @Autowired
+    Services services;
+
+    /**
+     * (USER) Balance Check
+     * @param secretKey
+     * @return
+     */
+    @GetMapping("/")
+    public String getBalance(@RequestParam(name = "secretKey") String secretKey) {
+        List<Wallet> wallets = services.wallet.getAllBySecretKey(secretKey);
+
+        return walletsToString(wallets);
+    }
+
+    /**
+     * (USER) Balance Top Up
+     * @param request
+     * @return
+     */
+    @PostMapping("/top-up")
+    public String topUp(@RequestBody TopUpRequest request) {
+        System.out.println(request);
+        Wallet wallet = services.wallet.getMainBySecretKey(request.secret_key);
+
+        return changeWalletBalance(wallet, Double.parseDouble(request.RUB_wallet));
+    }
+
+    /**
+     * (USER) Balance Withdraw Card
+     * @param request
+     * @return
+     */
+    @PostMapping("/withdraw/card")
+    public String withdraw(@RequestBody WithdrawCardRequest request) {
+        System.out.println("Снятие средств на карту");
+
+        Wallet wallet = services.wallet.getMainBySecretKey(request.secret_key);
+        double count = Double.parseDouble(request.count);
+
+        if (wallet.getCount() < count) {
+            return "{ \"response\": \"not enough money\"}";
+        }
+
+        return changeWalletBalance(wallet, -count);
+    }
+
+    /**
+     * (USER) Balance Withdraw Wallet
+     * @param request
+     * @return
+     */
+    @PostMapping("/withdraw/wallet")
+    public String withdraw(@RequestBody WithdrawWalletRequest request) {
+        System.out.println("Снятие средств на кошелёк");
+        Long currencyId = services.currency.getByName(request.currency).getId();
+
+        Wallet wallet =
+                services.wallet.getBySecretKeyAndCurrencyId(request.secret_key, currencyId);
+        double count = Double.parseDouble(request.count);
+
+        if (wallet.getCount() < count) {
+            return "{ \"response\": \"not enough money\"}";
+        }
+
+        return changeWalletBalance(wallet, -count);
+    }
+
+    /**
+     * (USER) Balance Exchange Currency
+     * @param request
+     * @return
+     */
+    @PostMapping("/exchange")
+    public String exchange(@RequestBody ExchangeCurrencyRequest request) {
+        Long currencyIdFrom = services.currency.getByName(request.currency_from).getId();
+        Long currencyIdTo = services.currency.getByName(request.currency_to).getId();
+        Double countFrom = Double.parseDouble(request.amount);
+
+        // проверка на воЗможность снять нужную сумму с 1-го кошелька
+        Wallet walletFrom = services.wallet.getBySecretKeyAndCurrencyId(request.secret_key, currencyIdFrom);
+        if (walletFrom.getCount() < countFrom) {
+            return exceptionResponse();
+        }
+
+        // переводим 1-ую валюту во 2-ую
+        Curse curse = services.curse.getByCurrenciesIds(currencyIdFrom, currencyIdTo);
+        double countTo;
+        if (curse.getCurrencyIdFrom().equals(currencyIdFrom)) {
+            countTo = countFrom / curse.getCount();
+        } else {
+            countTo = countFrom * curse.getCount();
+        }
+
+        // снимаем сумму с кошелька 1-ой валюты
+        changeWalletBalance(walletFrom, -countFrom);
+
+        // кладём сумму на кошелёк 2-ой валюты (и соЗдаем его, если до этого не соЗдавался)
+        Wallet walletTo = services.wallet.getBySecretKeyAndCurrencyId(request.secret_key, currencyIdTo);
+        if (walletTo == null) {
+            services.wallet.saveWallet(new Wallet(request.secret_key, currencyIdTo, countTo));
+            return "{ \"" + request.currency_to + "_wallet\": \"" + countTo + "\"}";
+        } else {
+            return changeWalletBalance(walletTo, countTo);
+        }
+    }
+
+
+
+    private String walletsToString(List<Wallet> wallets) {
+        StringBuilder sb = new StringBuilder();
+        sb.append('{');
+        sb.append('\n');
+        for (int i = 0; i < wallets.size(); i++) {
+            Currency currency = services.currency.getById(wallets.get(i).getCurrencyId());
+            sb.append('"');
+            sb.append(currency.getName());
+            sb.append("_wallet\": ");
+            sb.append('"');
+            sb.append(wallets.get(i).getCount());
+            sb.append('"');
+            sb.append('\n');
+        }
+        sb.append('}');
+
+        return sb.toString();
+    }
+
+    private String changeWalletBalance(Wallet wallet, double count) {
+        wallet.setCount(wallet.getCount() + count);
+        services.wallet.saveWallet(wallet);
+
+        Currency currency = services.currency.getById(wallet.getCurrencyId());
+        return "{ \"" + currency.getName() + "_wallet\": \"" + wallet.getCount() + "\"}";
+    }
+
+    private String exceptionResponse() {
+        return "{ \"response\": \"not enough money on wallet\"}";
+    }
+}
--- src/main/java/ru/relex/rozhnovL/controller/UserController.java
+++ src/main/java/ru/relex/rozhnovL/controller/UserController.java
@@ -5,14 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.relex.rozhnovL.entity.Curse;
-import ru.relex.rozhnovL.requests.RegistrationRequest;
-import ru.relex.rozhnovL.requests.topUpRequest;
+import ru.relex.rozhnovL.requests.*;
 import ru.relex.rozhnovL.Services;
-import ru.relex.rozhnovL.entity.Currency;
 import ru.relex.rozhnovL.entity.User;
-import ru.relex.rozhnovL.entity.Wallet;
-import ru.relex.rozhnovL.requests.withdrawCardRequest;
-import ru.relex.rozhnovL.requests.withdrawWalletRequest;
 
 import java.util.List;
 
@@ -35,81 +30,16 @@ public class UserController {
         String secretKey = Generator.generateSecretKey(request.username, request.email);
 
         User newUser = new User(request.username, request.email, secretKey, false);
-        services.userService.saveUser(newUser);
+        services.user.saveUser(newUser);
         return "{ \"secret_key\": \"" + secretKey + "\"}";
-    }
-
-    /**
-     * (USER) Balance Check
-     * @param secretKey
-     * @return
-     */
-    @GetMapping("/balance")
-    public String getBalance(@RequestParam(name = "secretKey") String secretKey) {
-        List<Wallet> wallets = services.walletService.getAllBySecretKey(secretKey);
-
-        return walletsToString(wallets);
-    }
-
-    /**
-     * (USER) Balance Top Up
-     * @param request
-     * @return
-     */
-    @PostMapping("/balance/top-up")
-    public String topUp(@RequestBody topUpRequest request) {
-        System.out.println(request);
-        Wallet wallet = services.walletService.getMainBySecretKey(request.secret_key);
-
-        return changeWalletBalance(wallet, Double.parseDouble(request.RUB_wallet));
-    }
-
-    /**
-     * (USER) Balance Withdraw Card
-     * @param request
-     * @return
-     */
-    @PostMapping("/balance/withdraw/card")
-    public String withdraw(@RequestBody withdrawCardRequest request) {
-        System.out.println("Снятие средств на карту");
-
-        Wallet wallet = services.walletService.getMainBySecretKey(request.secret_key);
-        double count = Double.parseDouble(request.count);
-
-        if (wallet.getCount() < count) {
-            return "{ \"response\": \"not enough money\"}";
-        }
-
-        return changeWalletBalance(wallet, -count);
-    }
-
-    /**
-     * (USER) Balance Withdraw Wallet
-     * @param request
-     * @return
-     */
-    @PostMapping("/balance/withdraw/wallet")
-    public String withdraw(@RequestBody withdrawWalletRequest request) {
-        System.out.println("Снятие средств на кошелёк");
-        Long currencyId = services.currencyService.getByName(request.currency).getId();
-
-        Wallet wallet =
-                services.walletService.getBySecretKeyAndCurrencyId(request.secret_key, currencyId);
-        double count = Double.parseDouble(request.count);
-
-        if (wallet.getCount() < count) {
-            return "{ \"response\": \"not enough money\"}";
-        }
-
-        return changeWalletBalance(wallet, -count);
     }
 
 
     @GetMapping("/curse")
     public String checkCurse(@RequestParam(name = "secret_key") String secretKey,
                              @RequestParam(name = "currency") String curCurrency) {
-        Long currencyId = services.currencyService.getByName(curCurrency).getId();
-        List<Curse> curses = services.curseService.getByCurrencyId(currencyId);
+        Long currencyId = services.currency.getByName(curCurrency).getId();
+        List<Curse> curses = services.curse.getByCurrencyId(currencyId);
 
         return cursesToString(curses, currencyId);
     }
@@ -121,7 +51,7 @@ public class UserController {
         sb.append("{\n");
 
         for (Curse curse : curses) {
-            String currencyName = services.currencyService.getById(curse.getCurrencyIdTo()).getName();
+            String currencyName = services.currency.getById(curse.getCurrencyIdTo()).getName();
             sb.append('"');
             sb.append(currencyName);
 
@@ -136,31 +66,5 @@ public class UserController {
         }
         sb.append('}');
         return sb.toString();
-    }
-    private String walletsToString(List<Wallet> wallets) {
-        StringBuilder sb = new StringBuilder();
-        sb.append('{');
-        sb.append('\n');
-        for (int i = 0; i < wallets.size(); i++) {
-            Currency currency = services.currencyService.getById(wallets.get(i).getCurrencyId());
-            sb.append('"');
-            sb.append(currency.getName());
-            sb.append("_wallet\": ");
-            sb.append('"');
-            sb.append(wallets.get(i).getCount());
-            sb.append('"');
-            sb.append('\n');
-        }
-        sb.append('}');
-
-        return sb.toString();
-    }
-
-    private String changeWalletBalance(Wallet wallet, double count) {
-        wallet.setCount(wallet.getCount() + count);
-        services.walletService.saveWallet(wallet);
-
-        Currency currency = services.currencyService.getById(wallet.getCurrencyId());
-        return "{ \"" + currency.getName() + "_wallet\": \"" + wallet.getCount() + "\"}";
     }
 }
